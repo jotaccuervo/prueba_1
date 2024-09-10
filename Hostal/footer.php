d5106573acfbbff5283c696ef49998b2fe3f63ea
jotaccuervo <30586555+jotaccuervo@users.noreply.github.com>
2024-09-09 19:42:25 -0500

    Add files via upload

---
+++ Hostal/footer.php
@@ -0,0 +1,34 @@
+		<FOOTER>
+			<P>Este es el pie de página</P>
+			<DIV id="cajaEnlacesFooter">
+				<NAV>
+					<UL>
+						<LI><A href="#" title="Link 1">Enlace 1</A></LI>
+						<LI><A href="#" title="Link 2">Enlace 2</A></LI>
+						<LI><A href="#" title="Link 3">Enlace 3</A></LI>
+						<LI><A href="#" title="Link 4">Enlace 4</A></LI>
+					</UL>
+				</NAV>
+
+				<NAV>
+					<UL>
+						<LI><A href="#" title="Link 1">Enlace 1</A></LI>
+						<LI><A href="#" title="Link 2">Enlace 2</A></LI>
+						<LI><A href="#" title="Link 3">Enlace 3</A></LI>
+						<LI><A href="#" title="Link 4">Enlace 4</A></LI>
+					</UL>
+				</NAV>
+
+				<NAV>
+					<UL>
+						<LI><A href="#" title="Link 1">Enlace 1</A></LI>
+						<LI><A href="#" title="Link 2">Enlace 2</A></LI>
+						<LI><A href="#" title="Link 3">Enlace 3</A></LI>
+						<LI><A href="#" title="Link 4">Enlace 4</A></LI>
+					</UL>
+				</NAV>
+			</DIV>
+			>
+
+			<A id="subir" href="#titulo" rel="nofollow" title="Subir"><DIV>Subir</DIV></A>
+		</FOOTER>
